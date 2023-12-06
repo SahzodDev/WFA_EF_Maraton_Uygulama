a497a2e89fa7b0acafff0f25193f7780c30d124d
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2023-12-06 15:04:51 +0300

    albumrep

--- Plak.DAL/Repositories/AlbumRepository.cs
+++ Plak.DAL/Repositories/AlbumRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Plak.DAL.Context;
+using Plak.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,50 @@ using System.Threading.Tasks;
 
 namespace Plak.DAL.Repositories
 {
-    internal class AlbumRepository
+    public class AlbumRepository
     {
+        public AlbumRepository() 
+        {
+           dbContext =new AppDbContext();
+        }
+        AppDbContext dbContext;
+
+        public void Add(Album album)
+        {
+            dbContext.Albums.Add(album);
+            dbContext.SaveChanges();
+
+        }
+        public void Update(Album album)
+        {
+            album.Status = Domain.Enums.Status.Modified;
+            album.ModifiedDate = DateTime.Now;
+            dbContext.Albums.Update(album);
+            dbContext.SaveChanges();
+            
+
+        }
+        public void DeleteByStatus(int albumID)
+        {
+            Album album = dbContext.Albums.Find(albumID);
+            album.Status=Domain.Enums.Status.Deleted;
+            album.DeletedDate = DateTime.Now;
+            dbContext.Albums.Update(album);
+            dbContext.SaveChanges();
+
+        }
+        public void PermaDelete(int albumID)
+        {
+            Album album = dbContext.Albums.Find(albumID);
+            dbContext.Remove(album);
+            dbContext.SaveChanges();
+        }
+
+        public List <Album> GetAll()
+        {
+            return dbContext.Albums.ToList();
+        }
+        
     }
+
 }
