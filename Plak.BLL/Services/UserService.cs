9bf9d1c5889a2ab0c90667cb8b7c541b0a268dcc
Shohzod İrgaş <131816386+SahzodDev@users.noreply.github.com>
2023-12-06 15:21:55 +0300

    user service created

--- Plak.BLL/Services/UserService.cs
+++ Plak.BLL/Services/UserService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Plak.DAL.Repositories;
+using Plak.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +10,94 @@ namespace Plak.BLL.Services
 {
     public class UserService
     {
-        
+        public UserService()
+        {
+            repository = new UserRepository();
+        }
+        UserRepository repository;
+
+        public User GetUserByUsername(string username)
+        {
+            return repository.GetUserByUsername(username);
+        }
+
+        public User GetUserById(int userId)
+        {
+            return repository.GetUserById(userId);
+        }
+
+        public void AddStandardUser(User user) 
+        {
+            user.UserType = Domain.Enums.UserType.Standard;
+            repository.Add(user);
+        }
+
+        public void UpdateUser(User user)
+        {
+            repository.Update(user);
+        }
+
+        public void DeleteUser(User user) 
+        {
+            repository.Delete(user);
+        }
+
+        public bool CheckUsernameExists(string username) 
+        {
+            User user = repository.GetUserByUsername(username);
+            if (user != null) 
+            {
+                return true;
+            }
+            else 
+            { 
+                return false; 
+            }  
+        }
+
+        public bool CheckPassword(string password, User user)
+        {
+            if (!user.VerifyPassword(password)) 
+            {
+                return false;
+            }
+            else
+            { 
+                return true; 
+            }
+        }
+
+        public void PasswordChange(string password, User user)
+        {
+            user.ChangePassword(password);
+            repository.Update(user);
+        }
+
+        public void SignUp(User user) 
+        {
+            if (!CheckUsernameExists(user.Username)) 
+            {
+                throw new Exception("Kullanıcı adı mevcut.");
+            }
+
+            repository.Add(user);
+        }
+
+        public User UserLogin(string username, string password)
+        {
+            User user = repository.GetUserByUsername(username);
+            if (user == null) 
+            {
+                throw new Exception("Kullanıcı bulunamadı.");
+            }
+
+            if (!user.VerifyPassword(password))
+            {
+                throw new Exception("Şifre yanlış.");
+            }
+
+            return user;
+        }
     }
 
 
