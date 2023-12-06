521e2b7461610e069a52674987a6e9f62d69169d
Shohzod İrgaş <131816386+SahzodDev@users.noreply.github.com>
2023-12-06 14:18:33 +0300

    appdbcontext created

---
+++ Plak.DAL/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Plak.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plak.DAL.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = DESKTOP-EVCENE1; Initial Catalog = DietDb31; User Id = sa; Password = Anyela123");
+            //base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
