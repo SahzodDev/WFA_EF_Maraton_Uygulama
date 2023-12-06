75e3ee163f34141634d336940eedc56d029238f1
reyhangunduz <144460500+reyhangunduz@users.noreply.github.com>
2023-12-06 14:15:19 +0300

    LoginScreen ve MainScreen yapıldı

---
+++ WFA_EF_Maraton_Uygulama/MainScreen.Designer.cs
@@ -0,0 +1,424 @@
+﻿namespace Plak.UI
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cbDurum = new ComboBox();
+            numUcret = new NumericUpDown();
+            numIndirim = new NumericUpDown();
+            dtpYayinTarihi = new DateTimePicker();
+            lblArtistGrup = new Label();
+            lblDurum = new Label();
+            lblIndirim = new Label();
+            txtArtistGrup = new TextBox();
+            lblUcret = new Label();
+            lblYayinTarihi = new Label();
+            lblAd = new Label();
+            txtAd = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            lbIndirimli = new ListBox();
+            lbEklenen10 = new ListBox();
+            lbSatisDevam = new ListBox();
+            lbSatisDurduruldu = new ListBox();
+            lvDetails = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            columnHeader6 = new ColumnHeader();
+            btnAlbumGuncelle = new Button();
+            btnAlbumKaydet = new Button();
+            btnAlbumSil = new Button();
+            btnYeniAlbum = new Button();
+            lblDetay = new Label();
+            linklblSifreDegistir = new LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)numUcret).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numIndirim).BeginInit();
+            SuspendLayout();
+            // 
+            // cbDurum
+            // 
+            cbDurum.FormattingEnabled = true;
+            cbDurum.Location = new Point(372, 669);
+            cbDurum.Margin = new Padding(3, 4, 3, 4);
+            cbDurum.Name = "cbDurum";
+            cbDurum.Size = new Size(172, 28);
+            cbDurum.TabIndex = 75;
+            // 
+            // numUcret
+            // 
+            numUcret.Location = new Point(86, 707);
+            numUcret.Margin = new Padding(3, 4, 3, 4);
+            numUcret.Name = "numUcret";
+            numUcret.Size = new Size(173, 27);
+            numUcret.TabIndex = 74;
+            // 
+            // numIndirim
+            // 
+            numIndirim.Location = new Point(372, 627);
+            numIndirim.Margin = new Padding(3, 4, 3, 4);
+            numIndirim.Name = "numIndirim";
+            numIndirim.Size = new Size(173, 27);
+            numIndirim.TabIndex = 73;
+            // 
+            // dtpYayinTarihi
+            // 
+            dtpYayinTarihi.Location = new Point(86, 665);
+            dtpYayinTarihi.Margin = new Padding(3, 4, 3, 4);
+            dtpYayinTarihi.Name = "dtpYayinTarihi";
+            dtpYayinTarihi.Size = new Size(172, 27);
+            dtpYayinTarihi.TabIndex = 72;
+            // 
+            // lblArtistGrup
+            // 
+            lblArtistGrup.AutoSize = true;
+            lblArtistGrup.Location = new Point(287, 707);
+            lblArtistGrup.Name = "lblArtistGrup";
+            lblArtistGrup.Size = new Size(82, 20);
+            lblArtistGrup.TabIndex = 71;
+            lblArtistGrup.Text = "Artist/Grup";
+            // 
+            // lblDurum
+            // 
+            lblDurum.AutoSize = true;
+            lblDurum.Location = new Point(287, 669);
+            lblDurum.Name = "lblDurum";
+            lblDurum.Size = new Size(54, 20);
+            lblDurum.TabIndex = 70;
+            lblDurum.Text = "Durum";
+            // 
+            // lblIndirim
+            // 
+            lblIndirim.AutoSize = true;
+            lblIndirim.Location = new Point(287, 630);
+            lblIndirim.Name = "lblIndirim";
+            lblIndirim.Size = new Size(56, 20);
+            lblIndirim.TabIndex = 69;
+            lblIndirim.Text = "İndirim";
+            // 
+            // txtArtistGrup
+            // 
+            txtArtistGrup.Location = new Point(372, 703);
+            txtArtistGrup.Margin = new Padding(3, 4, 3, 4);
+            txtArtistGrup.Name = "txtArtistGrup";
+            txtArtistGrup.Size = new Size(172, 27);
+            txtArtistGrup.TabIndex = 68;
+            // 
+            // lblUcret
+            // 
+            lblUcret.AutoSize = true;
+            lblUcret.Location = new Point(2, 710);
+            lblUcret.Name = "lblUcret";
+            lblUcret.Size = new Size(44, 20);
+            lblUcret.TabIndex = 67;
+            lblUcret.Text = "Ücret";
+            // 
+            // lblYayinTarihi
+            // 
+            lblYayinTarihi.AutoSize = true;
+            lblYayinTarihi.Location = new Point(2, 670);
+            lblYayinTarihi.Name = "lblYayinTarihi";
+            lblYayinTarihi.Size = new Size(82, 20);
+            lblYayinTarihi.TabIndex = 66;
+            lblYayinTarihi.Text = "Yayın Tarihi";
+            // 
+            // lblAd
+            // 
+            lblAd.AutoSize = true;
+            lblAd.Location = new Point(2, 626);
+            lblAd.Name = "lblAd";
+            lblAd.Size = new Size(28, 20);
+            lblAd.TabIndex = 65;
+            lblAd.Text = "Ad";
+            // 
+            // txtAd
+            // 
+            txtAd.Location = new Point(86, 622);
+            txtAd.Margin = new Padding(3, 4, 3, 4);
+            txtAd.Name = "txtAd";
+            txtAd.Size = new Size(172, 27);
+            txtAd.TabIndex = 64;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(806, 345);
+            label5.Name = "label5";
+            label5.Size = new Size(64, 20);
+            label5.TabIndex = 63;
+            label5.Text = "İndirimli";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(582, 345);
+            label4.Name = "label4";
+            label4.Size = new Size(113, 20);
+            label4.TabIndex = 62;
+            label4.Text = "Son Eklenen 10 ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(801, 23);
+            label3.Name = "label3";
+            label3.Size = new Size(194, 20);
+            label3.TabIndex = 61;
+            label3.Text = "Satış Durumu Devam Ediyor";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(582, 23);
+            label2.Name = "label2";
+            label2.Size = new Size(176, 20);
+            label2.TabIndex = 60;
+            label2.Text = "Satış Durumu Durduruldu";
+            // 
+            // lbIndirimli
+            // 
+            lbIndirimli.FormattingEnabled = true;
+            lbIndirimli.ItemHeight = 20;
+            lbIndirimli.Location = new Point(806, 369);
+            lbIndirimli.Margin = new Padding(3, 4, 3, 4);
+            lbIndirimli.Name = "lbIndirimli";
+            lbIndirimli.Size = new Size(189, 264);
+            lbIndirimli.TabIndex = 59;
+            // 
+            // lbEklenen10
+            // 
+            lbEklenen10.FormattingEnabled = true;
+            lbEklenen10.ItemHeight = 20;
+            lbEklenen10.Location = new Point(582, 369);
+            lbEklenen10.Margin = new Padding(3, 4, 3, 4);
+            lbEklenen10.Name = "lbEklenen10";
+            lbEklenen10.Size = new Size(189, 264);
+            lbEklenen10.TabIndex = 58;
+            // 
+            // lbSatisDevam
+            // 
+            lbSatisDevam.FormattingEnabled = true;
+            lbSatisDevam.ItemHeight = 20;
+            lbSatisDevam.Location = new Point(801, 47);
+            lbSatisDevam.Margin = new Padding(3, 4, 3, 4);
+            lbSatisDevam.Name = "lbSatisDevam";
+            lbSatisDevam.Size = new Size(199, 264);
+            lbSatisDevam.TabIndex = 57;
+            // 
+            // lbSatisDurduruldu
+            // 
+            lbSatisDurduruldu.FormattingEnabled = true;
+            lbSatisDurduruldu.ItemHeight = 20;
+            lbSatisDurduruldu.Location = new Point(582, 47);
+            lbSatisDurduruldu.Margin = new Padding(3, 4, 3, 4);
+            lbSatisDurduruldu.Name = "lbSatisDurduruldu";
+            lbSatisDurduruldu.Size = new Size(189, 264);
+            lbSatisDurduruldu.TabIndex = 56;
+            // 
+            // lvDetails
+            // 
+            lvDetails.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5, columnHeader6 });
+            lvDetails.FullRowSelect = true;
+            lvDetails.GridLines = true;
+            lvDetails.Location = new Point(19, 49);
+            lvDetails.Margin = new Padding(3, 4, 3, 4);
+            lvDetails.Name = "lvDetails";
+            lvDetails.Size = new Size(524, 519);
+            lvDetails.TabIndex = 55;
+            lvDetails.UseCompatibleStateImageBehavior = false;
+            lvDetails.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Ad";
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Artist/Grup";
+            columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Yayın Tarihi";
+            columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Ücret";
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "İndirim";
+            columnHeader5.Width = 100;
+            // 
+            // columnHeader6
+            // 
+            columnHeader6.Text = "Satış Durumu";
+            columnHeader6.Width = 100;
+            // 
+            // btnAlbumGuncelle
+            // 
+            btnAlbumGuncelle.Location = new Point(427, 775);
+            btnAlbumGuncelle.Margin = new Padding(3, 4, 3, 4);
+            btnAlbumGuncelle.Name = "btnAlbumGuncelle";
+            btnAlbumGuncelle.Size = new Size(124, 52);
+            btnAlbumGuncelle.TabIndex = 54;
+            btnAlbumGuncelle.Text = "Albüm Güncelle";
+            btnAlbumGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnAlbumKaydet
+            // 
+            btnAlbumKaydet.Location = new Point(290, 775);
+            btnAlbumKaydet.Margin = new Padding(3, 4, 3, 4);
+            btnAlbumKaydet.Name = "btnAlbumKaydet";
+            btnAlbumKaydet.Size = new Size(118, 52);
+            btnAlbumKaydet.TabIndex = 53;
+            btnAlbumKaydet.Text = "Albüm Kaydet";
+            btnAlbumKaydet.UseVisualStyleBackColor = true;
+            // 
+            // btnAlbumSil
+            // 
+            btnAlbumSil.Location = new Point(154, 775);
+            btnAlbumSil.Margin = new Padding(3, 4, 3, 4);
+            btnAlbumSil.Name = "btnAlbumSil";
+            btnAlbumSil.Size = new Size(118, 52);
+            btnAlbumSil.TabIndex = 52;
+            btnAlbumSil.Text = "Albüm Sil";
+            btnAlbumSil.UseVisualStyleBackColor = true;
+            // 
+            // btnYeniAlbum
+            // 
+            btnYeniAlbum.Location = new Point(19, 775);
+            btnYeniAlbum.Margin = new Padding(3, 4, 3, 4);
+            btnYeniAlbum.Name = "btnYeniAlbum";
+            btnYeniAlbum.Size = new Size(118, 52);
+            btnYeniAlbum.TabIndex = 51;
+            btnYeniAlbum.Text = "Yeni Albüm";
+            btnYeniAlbum.UseVisualStyleBackColor = true;
+            // 
+            // lblDetay
+            // 
+            lblDetay.AutoSize = true;
+            lblDetay.Location = new Point(19, 25);
+            lblDetay.Name = "lblDetay";
+            lblDetay.Size = new Size(48, 20);
+            lblDetay.TabIndex = 50;
+            lblDetay.Text = "Detay";
+            // 
+            // linklblSifreDegistir
+            // 
+            linklblSifreDegistir.AutoSize = true;
+            linklblSifreDegistir.Location = new Point(881, 811);
+            linklblSifreDegistir.Name = "linklblSifreDegistir";
+            linklblSifreDegistir.Size = new Size(95, 20);
+            linklblSifreDegistir.TabIndex = 49;
+            linklblSifreDegistir.TabStop = true;
+            linklblSifreDegistir.Text = "Şifre Değiştir";
+            // 
+            // MainScreen
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1143, 842);
+            Controls.Add(cbDurum);
+            Controls.Add(numUcret);
+            Controls.Add(numIndirim);
+            Controls.Add(dtpYayinTarihi);
+            Controls.Add(lblArtistGrup);
+            Controls.Add(lblDurum);
+            Controls.Add(lblIndirim);
+            Controls.Add(txtArtistGrup);
+            Controls.Add(lblUcret);
+            Controls.Add(lblYayinTarihi);
+            Controls.Add(lblAd);
+            Controls.Add(txtAd);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(lbIndirimli);
+            Controls.Add(lbEklenen10);
+            Controls.Add(lbSatisDevam);
+            Controls.Add(lbSatisDurduruldu);
+            Controls.Add(lvDetails);
+            Controls.Add(btnAlbumGuncelle);
+            Controls.Add(btnAlbumKaydet);
+            Controls.Add(btnAlbumSil);
+            Controls.Add(btnYeniAlbum);
+            Controls.Add(lblDetay);
+            Controls.Add(linklblSifreDegistir);
+            Name = "MainScreen";
+            Text = "MainScreen";
+            Load += MainScreen_Load;
+            ((System.ComponentModel.ISupportInitialize)numUcret).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numIndirim).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cbDurum;
+        private NumericUpDown numUcret;
+        private NumericUpDown numIndirim;
+        private DateTimePicker dtpYayinTarihi;
+        private Label lblArtistGrup;
+        private Label lblDurum;
+        private Label lblIndirim;
+        private TextBox txtArtistGrup;
+        private Label lblUcret;
+        private Label lblYayinTarihi;
+        private Label lblAd;
+        private TextBox txtAd;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private ListBox lbIndirimli;
+        private ListBox lbEklenen10;
+        private ListBox lbSatisDevam;
+        private ListBox lbSatisDurduruldu;
+        private ListView lvDetails;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private Button btnAlbumGuncelle;
+        private Button btnAlbumKaydet;
+        private Button btnAlbumSil;
+        private Button btnYeniAlbum;
+        private Label lblDetay;
+        private LinkLabel linklblSifreDegistir;
+    }
+}
