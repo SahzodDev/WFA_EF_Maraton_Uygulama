4493b7629cbac61729ef18a8af8eaba7e8d2d427
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2023-12-06 13:53:28 +0300

    salesstate

---
+++ Plak.Domain/Enums/SalesState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plak.Domain.Enums
+{
+    public enum SalesState
+    {
+        Ongoing=1, Stopped=2
+    }
+}
