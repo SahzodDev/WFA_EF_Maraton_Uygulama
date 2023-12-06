5eb17b70dbeede75af5470d546c7f82c02674682
Shohzod İrgaş <131816386+SahzodDev@users.noreply.github.com>
2023-12-06 14:35:09 +0300

    dbcdsgsgd

---
+++ Plak.DAL/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Plak.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plak.DAL.Context
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Album> Albums { get; set; }
+        DbSet<Artist> Artists { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = DESKTOP-EVCENE1; Initial Catalog = AlbumDb; User Id = sa; Password = Anyela123");
+            //base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
