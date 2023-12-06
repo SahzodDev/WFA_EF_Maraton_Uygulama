0613847e8285ccf0d4f795fe56c0641f9ca86c7a
Shohzod İrgaş <131816386+SahzodDev@users.noreply.github.com>
2023-12-06 15:23:58 +0300

    artist service created

--- Plak.BLL/Services/ArtistService.cs
+++ Plak.BLL/Services/ArtistService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Plak.DAL.Repositories;
+using Plak.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,31 @@ namespace Plak.BLL.Services
 {
     public class ArtistService
     {
+        public ArtistService()
+        {
+            repository = new ArtistRepository();
+        }
+        ArtistRepository repository;
+
+        public void Add(Artist artist)
+        {
+            repository.Add(artist);
+        }
+
+        public void Update(Artist artist)
+        {
+            repository.Update(artist);
+        }
+
+        public void Delete(Artist artist)
+        {
+            repository.Delete(artist);
+        }
+
+        public List<Artist> GetAll()
+        {
+            return repository.GetAll();
+        }
+
     }
 }
